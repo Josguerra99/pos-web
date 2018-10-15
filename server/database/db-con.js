a4911a43c52b8a50b42a712d0bdeeb9b3f096d5d
jose <josguerra99@gmail.com>
2018-10-15 03:38:56 -0600

    Heroku - react test 3

--- server/database/db-con.js
+++ server/database/db-con.js
@@ -2,10 +2,19 @@
 const mysql = require("mysql");
 
 const con = mysql.createConnection({
+  host: "sql9.freemysqlhosting.net",
+  database: "sql9261239",
+  user: "sql9261239",
+  password: "Rb2JzKiQ2P",
+  multipleStatements: true
+});
+/*
+const con = mysql.createConnection({
   host: "localhost",
   database: "posdb",
   user: "root",
   password: "",
   multipleStatements: true
-});
+}); 
+*/
 module.exports = con;
